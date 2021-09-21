38ed5edd3725c239669da93f7701bed05c3f7ac0
Arjun Vijay <arjunvjy95@gmail.com>
2021-09-21 14:54:26 +0530

    After model

--- CleanArch/CleanArch.Domain/Models/Course.cs
+++ CleanArch/CleanArch.Domain/Models/Course.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace CleanArch.Domain.Models
 {
-    public class Course
+    class Course
     {
         public int Id { get; set; }
         public string Name { get; set; }
