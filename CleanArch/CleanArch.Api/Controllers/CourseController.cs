1f8dc1ffbc04a2fd7a9e8eb9b39e5060d380b5db
Arjun Vijay <arjunvjy95@gmail.com>
2021-09-23 11:43:07 +0530

    After CQRS

--- CleanArch/CleanArch.Api/Controllers/CourseController.cs
+++ CleanArch/CleanArch.Api/Controllers/CourseController.cs
@@ -1,15 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CleanArch.Api.Controllers
 {
-    [ApiController]
     [Route("api/[controller]")]
+    [ApiController]
     public class CourseController : ControllerBase
     {
+        private readonly ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CourseViewModel courseViewModel)
+        {
+            _courseService.Create(courseViewModel);
+
+            return Ok(courseViewModel);
+        }
     }
 }
